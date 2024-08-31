682b3e193991c5c9c89fa9d559bb1b934358d284
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-08-31 00:31:47 -0300

    style(inputVideoChecked): modify lang en-US

--- src/components/input/InputVideoChecked.tsx
+++ src/components/input/InputVideoChecked.tsx
@@ -5,7 +5,7 @@ export function InputVideoChecked() {
     <div className="border-2 border-[#00ff00] rounded-lg w-full py-4 flex items-center justify-center gap-3">
       <Check size={34} color="#00ff00" />
       <p className="font-bold text-sm">
-        Vídeo carregado com <span className="text-[#00ff00]">sucesso!</span>
+        Video uploaded <span className="text-[#00ff00]">successfully!</span>
       </p>
     </div>
   )
