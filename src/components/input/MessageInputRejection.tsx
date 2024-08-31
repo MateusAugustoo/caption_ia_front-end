5d6436474fdbf17dcfdeb495d1c7c41004000245
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-08-31 00:32:34 -0300

    style(MessageInputRejection): modify lang en-US

--- src/components/input/MessageInputRejection.tsx
+++ src/components/input/MessageInputRejection.tsx
@@ -5,7 +5,7 @@ export function MessageRejection() {
     <p className="text-red-500 flex gap-1 items-center">
       <X color="#ef4444" size={16} />
       <span className="font-medium italic">
-        O formato do arquivo deve ser .mp4 ou .mkv
+        The file format must be.mp4 or.mkv
       </span>
     </p>
   )
